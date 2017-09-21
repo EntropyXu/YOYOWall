b18a5aacaa2308d18dc8c06867fbdd32b4b7129a
EntropyXu <funtomxu@gmail.com>
2017-09-21 18:08:45 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YOYOWall::Application.config.secret_token = 'aaad993e9bad55ad74835ad93d8cdb81f85e27a47cc1c01c0b46d93b2fb50ad7a026eb1d8e0e1d264a515f6ca69fa86118f3687913b396e886d6317add5155bf'
